e043bd395ad31f8fa05f97e975150c2d89c54c14
Bogdan Pastiu <v-bpastiu@digitalocean.com>
2022-11-22 11:11:33 +0200

    Addresses PR comments

--- github-telemetry/github-telemetry.py
+++ github-telemetry/github-telemetry.py
@@ -4,48 +4,51 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
-repository = "digitalocean/k8s-adoption-journey"
-token = os.getenv('GITHUB_TOKEN')
+REPOSITORY = "digitalocean/k8s-adoption-journey"
+MANDATORY_ENV_VAR = "GITHUB_TOKEN"
+
+if MANDATORY_ENV_VAR not in os.environ:
+        raise EnvironmentError(f"Failed because {MANDATORY_ENV_VAR} is not set.")
+
+token = os.getenv("GITHUB_TOKEN")
 views_headers = ["TotalDayViewCount", "UniqueViews", "Timestamp"]
 clones_headers = ["Clones", "UniqueClones", "Timestamp"]
 
-g = Github(token)
-
-repo = g.get_repo(repository)
-
-views = repo.get_views_traffic(per="day")
-clones = repo.get_clones_traffic(per="day")
-
-fileEmpty = os.stat("views.csv").st_size == 0
-
-if __name__ == "__main__":
-
-    with open('views.csv', 'a') as file_a:
-        writer = csv.DictWriter(file_a, delimiter=',', fieldnames=views_headers)
-        if fileEmpty:
+def write_to_csv(csv_name, csv_headers, telemetry_data, data_dict, first_colum_name, second_column_name):
+    """Creates separate CSVs with Github views and counts information"""
+    fileExists = os.path.isfile(csv_name)
+    with open(csv_name, 'a') as file:
+        writer = csv.DictWriter(file, delimiter=',', fieldnames=csv_headers)
+        if not fileExists:
             writer.writeheader()
-        for view in views['views']:
-            writer.writerow({'TotalDayViewCount': view.count, 'UniqueViews': view.uniques, 'Timestamp': view.timestamp.strftime("%m-%d-%Y")})
+        for data in telemetry_data[data_dict]:
+            writer.writerow({first_colum_name: data.count, second_column_name: data.uniques, 'Timestamp': data.timestamp.strftime("%m-%d-%Y")})
 
-    with open('clones.csv', 'a') as file_b:
-        writer = csv.DictWriter(file_b, delimiter=',', fieldnames=clones_headers)
-        if fileEmpty:
-            writer.writeheader()
-        for clone in clones['clones']:
-            writer.writerow({'Clones': clone.count, 'UniqueClones': clone.uniques, 'Timestamp': clone.timestamp.strftime("%m-%d-%Y")})
+
+def remove_duplicate_views(csv, duplicate_column):
+    """Removes duplicate data based on the timestamp column"""
+    remove_duplicates_views = pd.read_csv(csv)
+    no_duplicates_views = remove_duplicates_views.drop_duplicates(subset=[duplicate_column],keep='first')
+    no_duplicates_views.to_csv(csv, index=False)
+    return csv
+
+def create_data_frame(csv):
+    """Creates the data frame which will be used to create images"""
+    data_frame = pd.read_csv(csv)
+    return data_frame
+
+if __name__ == "__main__":  
+    g = Github(token)
+    repo = g.get_repo(REPOSITORY)
+    views = repo.get_views_traffic(per="day")
+    clones = repo.get_clones_traffic(per="day")
+    
+    write_to_csv('views.csv', views_headers, views, 'views' , 'TotalDayViewCount', 'UniqueViews')
+    write_to_csv('clones.csv', clones_headers, clones, 'clones' , 'Clones', 'UniqueClones')
+
+    remove_duplicate_views('views.csv', 'Timestamp')
+    remove_duplicate_views('clones.csv', 'Timestamp')
         
-
-    def remove_duplicate_views(csv, duplicate_column):
-        remove_duplicates_views = pd.read_csv(csv)
-        no_duplicates_views = remove_duplicates_views.drop_duplicates(subset=[duplicate_column],keep='first')
-        no_duplicates_views.to_csv(csv, index=False)
-        return csv
-
-
-    def create_data_frame(csv):
-        data_frame = pd.read_csv(csv)
-        return data_frame
-
     df_views = pd.DataFrame(create_data_frame('views.csv'))
     df_views.plot(x="Timestamp", y="TotalDayViewCount", kind='bar')
     plt.savefig('plot-views.png', dpi=300, bbox_inches='tight')
@@ -54,8 +57,6 @@ if __name__ == "__main__":
     df_clones.plot(x="Timestamp", y="Clones", kind='bar')
     plt.savefig('plot-clones.png', dpi=300, bbox_inches='tight')
 
-    remove_duplicate_views('views.csv', 'Timestamp')
-    remove_duplicate_views('clones.csv', 'Timestamp')
 
     
 
