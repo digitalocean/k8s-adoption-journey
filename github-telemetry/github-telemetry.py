890c65db37bbbe15464227ebf6e36afba49c7e27
Bogdan Pastiu <v-bpastiu@digitalocean.com>
2022-11-23 09:17:39 +0200

    Adds latest changes

--- github-telemetry/github-telemetry.py
+++ github-telemetry/github-telemetry.py
@@ -4,15 +4,11 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
-REPOSITORY = "digitalocean/k8s-adoption-journey"
-GITHUB_TOKEN = "GITHUB_TOKEN"
 
-if GITHUB_TOKEN not in os.environ:
-        raise EnvironmentError(f"Failed because {GITHUB_TOKEN} is not set.")
+TARGET_REPOSITORY = "digitalocean/k8s-adoption-journey"
+VIEWS_HEADERS = ["TotalDayViewCount", "UniqueViews", "Timestamp"]
+CLONES_HEADERS = ["Clones", "UniqueClones", "Timestamp"]
 
-token = os.getenv("GITHUB_TOKEN")
-views_headers = ["TotalDayViewCount", "UniqueViews", "Timestamp"]
-clones_headers = ["Clones", "UniqueClones", "Timestamp"]
 
 def write_to_csv(csv_name, csv_headers, telemetry_data, data_dict, first_colum_name, second_column_name):
     """Creates separate CSVs with Github views and counts information"""
@@ -37,28 +33,30 @@ def create_data_frame(csv):
     data_frame = pd.read_csv(csv)
     return data_frame
 
-if __name__ == "__main__":  
-    g = Github(token)
-    repo = g.get_repo(REPOSITORY)
+
+if __name__ == "__main__":
+    if "GITHUB_TOKEN" not in os.environ:
+        raise EnvironmentError("GITHUB_TOKEN environment variable is not set!")
+        
+    g = Github(os.getenv("GITHUB_TOKEN"))
+    repo = g.get_repo(TARGET_REPOSITORY)
     views = repo.get_views_traffic(per="day")
     clones = repo.get_clones_traffic(per="day")
     
-    write_to_csv('views.csv', views_headers, views, 'views' , 'TotalDayViewCount', 'UniqueViews')
-    write_to_csv('clones.csv', clones_headers, clones, 'clones' , 'Clones', 'UniqueClones')
+    write_to_csv('views.csv', VIEWS_HEADERS, views, 'views' , 'TotalDayViewCount', 'UniqueViews')
+    write_to_csv('clones.csv', CLONES_HEADERS, clones, 'clones' , 'Clones', 'UniqueClones')
 
     remove_duplicate_views('views.csv', 'Timestamp')
     remove_duplicate_views('clones.csv', 'Timestamp')
         
     df_views = pd.DataFrame(create_data_frame('views.csv'))
+    if df_views.empty == True:
+      raise EnvironmentError("Views CSV file has no data!")
     df_views.plot(x="Timestamp", y="TotalDayViewCount", kind='bar')
     plt.savefig('plot-views.png', dpi=300, bbox_inches='tight')
 
     df_clones = pd.DataFrame(create_data_frame('clones.csv'))
+    if df_clones.empty == True:
+      raise EnvironmentError("Clones CSV file has no data!")
     df_clones.plot(x="Timestamp", y="Clones", kind='bar')
     plt.savefig('plot-clones.png', dpi=300, bbox_inches='tight')
-
-
-    
-
-
-
